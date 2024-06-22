8f4d689ded2c693c13368893bae202ef21133cd8
Joao Padilha <jvlpadilha@outlook.com>
2024-06-22 15:26:55 -0300

    feat: sec3

--- src/app/course-card-old/course-card-old.component.ts
+++ src/app/course-card-old/course-card-old.component.ts
@@ -1,36 +1,45 @@
-import { Component, Input, Output } from '@angular/core';
-import { Course } from '../model/course';
-import { EventEmitter } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, Output } from "@angular/core";
+import { Course } from "../model/course";
+import { EventEmitter } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'course-card-old',
+  selector: "course-card-old",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './course-card-old.component.html',
-  styleUrl: './course-card-old.component.css'
+  templateUrl: "./course-card-old.component.html",
+  styleUrl: "./course-card-old.component.css",
 })
 export class CourseCardOldComponent {
   @Input()
-  course: Course
+  course: Course;
 
-  @Input({required: true})
-  index: number
+  @Input({ required: true })
+  index: number;
 
   @Output()
   courseSelected = new EventEmitter<Course>();
 
-  constructor(){}
-  ngOnInit(){
+  constructor() {}
+  ngOnInit() {}
 
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
   }
 
-  isImageVisible(){
-    return this.course && this.course.iconUrl
+  isBeginner() {
+    return this.course.category === "BEGINNER";
   }
 
-  onCourseViewed(){
-    this.courseSelected.emit(this.course)
-    console.log('oi - button')
+  onCourseViewed() {
+    this.courseSelected.emit(this.course);
+    console.log("oi - button");
+  }
+
+  cardStyle() {
+    return {
+      "background-image": "url(" + this.course.iconUrl + ")",
+      "background-size": "cover"
+    };
   }
 }
