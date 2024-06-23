2a42af99c85d0fe77b9f7451b1e23b27171f501e
Joao Padilha <jvlpadilha@outlook.com>
2024-06-23 13:06:56 -0300

    feat: view child and children

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,59 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { Course } from './model/course';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { Course } from "./model/course";
+import { CourseCardOldComponent } from "./course-card-old/course-card-old.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  courses: Course[] = COURSES;
+  courseItem: Course = COURSES[0];
 
-  courses: Course[] = COURSES
-  courseItem: Course = COURSES[0]
+  @ViewChild(CourseCardOldComponent, { read: ElementRef })
+  card: ElementRef;
 
-  onCourseSelected(course:Course){
-    console.log('oi - card', course)
+  @ViewChild(CourseCardOldComponent)
+  containerDiv: CourseCardComponent;
+
+  @ViewChildren(CourseCardOldComponent)
+  componentsChildren: QueryList<CourseCardComponent>;
+
+  onCourseSelected(course: Course) {
+    // console.log('', this.containerDiv)
+  }
+
+  ngAfterViewInit(): void {
+    this.componentsChildren.changes.subscribe((courses) =>
+      console.log(courses)
+    );
+  }
+
+  editCourse() {
+    this.courses.push({
+      id: 1,
+      description: "Angular Core Deep Dive",
+      iconUrl:
+        "https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png",
+      longDescription:
+        "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      category: "INTERMEDIATE",
+      lessonsCount: 10,
+    });
   }
 
   trackFunction(index: number, item: Course) {
-    
-      return item.description
+    return item.description;
   }
-
 }
